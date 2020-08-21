047edc17467227c83586ac13349fff591b316ca1
jjayeon <autumn.chiu.136@gmail.com>
2020-08-20 22:17:07 -0400

    just kidding, it works --- have to use the right keycode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import input from "./lib/Input.js"; // eslint-disable-line no-unused-vars
 import tests from "./lib/Tests.js"; // eslint-disable-line no-unused-vars
-import helper from "./lib/Helpers.js"; // eslint-disable-line no-unused-vars
 
 tests.test_bind(input);
-input.bind("Space", () => {
+input.bind(" ", () => {
   console.log("space pressed");
   tests.test_unbind(input);
 });
