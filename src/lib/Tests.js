9a8d6759b989211e9a92bf6424909e85450b641a
jjayeon <autumn.chiu.136@gmail.com>
2020-08-20 20:50:05 -0400

    added testing for x, y; functionality was added in an earlier commit that i forgot to mention

--- src/lib/Tests.js
+++ src/lib/Tests.js
@@ -1,5 +1,5 @@
 const Tests = {
-  test_pressed: function (input, testing) {
+  test_pressed: function (input, active) {
     function getAllPressed() {
       var out = "pressed: ";
       for (const key in input.pressed) {
@@ -9,10 +9,25 @@ const Tests = {
       }
       console.log(out);
     }
-    if (testing) {
+    if (active) {
       document.addEventListener("keydown", getAllPressed);
+      document.addEventListener("mousedown", getAllPressed);
     } else {
       document.removeEventListener("keydown", getAllPressed);
+      document.removeEventListener("mousedown", getAllPressed);
+    }
+  },
+
+  test_xy: function (input, active) {
+    function logPosition(e) {
+      console.log(`doc says: ${e.x}, ${e.y}`);
+      console.log(`input says: ${input.x}, ${input.y}`);
+    }
+
+    if (active) {
+      document.addEventListener("mousedown", logPosition);
+    } else {
+      document.removeEventListener("mousedown", logPosition);
     }
   },
 };
