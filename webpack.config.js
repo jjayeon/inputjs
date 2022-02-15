c743bc1cdaf846231deba6b5800f27e4c94396c6
jjayeon <autumn.chiu.136@gmail.com>
2022-02-15 11:17:22 -0500

    add gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,35 +4,35 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const PrettierWebpackPlugin = require("prettier-webpack-plugin");
 
 module.exports = {
-  devtool: "source-map",
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
+    devtool: "source-map",
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader",
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new CleanWebpackPlugin(),
+        new PrettierWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new CleanWebpackPlugin(),
-    new PrettierWebpackPlugin(),
-  ],
-  devServer: {
-    contentBase: "./dist",
-  },
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+    devServer: {
+        contentBase: "./build",
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "build"),
+    },
 };
